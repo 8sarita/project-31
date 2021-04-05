d3bbd8d285cb6120300ec727d0afc772d73abdcb
8sarita <74858829+8sarita@users.noreply.github.com>
2021-04-05 22:31:11 +0530

    Add files via upload

---
+++ divisions.js
@@ -0,0 +1,18 @@
+class Divisions {
+    constructor(x, y, w, h) {
+        var options = {
+
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.w = w;
+        this.h = h;
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill(random(50,250),random(100,250),random(20,200));
+        rect(pos.x, pos.y, this.w, this.h);
+    }
+};
